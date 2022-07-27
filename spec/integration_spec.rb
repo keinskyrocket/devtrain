18f79f30cf1509888d554c87d1a04df6738232d8
Keisuke Tanaka <keinskyrocket@KT.fritz.box>
2022-07-27 13:43:39 +1200

    Add tests for hangman method

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,19 +1,6 @@
 require_relative '../hangman'
 
 describe 'Hangman game integration test' do
-  new_game_property = {
-    hash_of_word: [
-      {letter: 't', validation: true},
-      {letter: 'u', validation: true},
-      {letter: 'r', validation: true},
-      {letter: 't', validation: true},
-      {letter: 'l', validation: true},
-      {letter: 'e', validation: true},
-    ],
-    game_won: true,
-    wrong_letters_answered: ['3', '/', '#', '*', '🤡']
-  }
-
   it 'Can win a game' do
     output = StringIO.new
     Hangman.new(build_input('turtle'.chars), output, 'turtle').play
@@ -60,16 +47,9 @@ describe 'Hangman game integration test' do
 
   it 'Does not allow non-letter guesses' do
     output = StringIO.new
-    Hangman.new(build_input('3/#*🤡turle'.chars), output, 'turtle').take_turn(new_game_property)
+    Hangman.new(build_input('3/#*🤡turle'.chars), output, 'turtle')
     expect(output.string).to include 'Only allow single letter guesses'
     expect(output.string).not_to include '>> Remaining lives: 8' 
-  end
-
-  it 'Does not allow more than 1 letter guessed' do
-    output = StringIO.new
-    Hangman.new(build_input('e4', 'gahgefawgaw3/#*🤡tr', 't', 'u', 'r', 'l', 'e'.chars), output, 'turtle').take_turn(new_game_property)
-    expect(output.string).to include 'Only allow single letter guesses'
-    expect(output.string).not_to include '>> Remaining lives: 8'
   end
 
   def build_input(*letters)
