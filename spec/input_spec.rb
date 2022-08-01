5c2d732228c5aba34f13426449a20bf68c92c90f
Keisuke Tanaka <keinskyrocket@KT.fritz.box>
2022-08-01 13:18:02 +1200

    Refactor test

--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -1,31 +1,40 @@
 require_relative '../input'
 
 describe Input do
+  let(:output) { StringIO.new }
   describe '#get_answer' do
     it 'should show the error message if typed other than "y" or "n"' do
-      output = StringIO.new
       Input.new(build_input('e4', 'gahgefawgaw3/#*🤡tr', 'n'.chars), output).get_answer
       expect(output.string).to include "Either 'y' or 'n' is allowed."
     end
 
     it 'should exit the game if typed "n"' do
-      output = StringIO.new
       Input.new(build_input('n'.chars), output).get_answer
       expect(output.string).to include "Bye"
     end
 
     it 'should restart the game if typed "y"' do
-      output = StringIO.new
       Input.new(build_input('y'.chars), output).get_answer
       expect(output.string).to include "Play again"
     end
 
     context 'when typed "y"' do
       it 'should restart the game' do
-        output = StringIO.new
         Input.new(build_input('y'.chars), output).get_answer
         expect(output.string).to include "Play again"
       end
+    end
+
+    def build_input(*letters)
+      StringIO.new(letters.join("\n") + "\n")
+    end
+  end
+
+  describe '#get_guess' do
+    it 'Does not allow non-letter guesses' do
+      Input.new(build_input('3/#*🤡turle'.chars), output).get_guess
+      expect(output.string).to include 'Only allow single letter guesses'
+      expect(output.string).not_to include '>> Remaining lives: 8' 
     end
 
     def build_input(*letters)
